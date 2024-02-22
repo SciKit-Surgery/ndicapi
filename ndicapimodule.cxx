86ae885a1b0221a76685991ce3e0c6c0577dec37
Stephen Thompson <s.thompson@ucl.ac.uk>
2024-02-22 09:07:21 +0000

    Issue #12 wrapped define in check python version

--- ndicapimodule.cxx
+++ ndicapimodule.cxx
@@ -27,7 +27,11 @@
   #define PyIntObject PyLongObject
   //#define PY_INT_OBJECT_OB_IVAL(ob) PyLong_AsLong((PyObject*)(ob))
   #define CYTHON_USE_PYLONG_INTERNALS 1 
-  #define PY_INT_OBJECT_OB_IVAL(ob) ob->long_value.ob_digit[0]
+  #if PY_MINOR_VERSION < 12
+    #define PY_INT_OBJECT_OB_IVAL(ob) ob->long_value.ob_digit[0]
+  #else
+    #define PY_INT_OBJECT_OB_IVAL(ob) ob->ob_digit[0]
+  #endif
   #define cmpfunc PyAsyncMethods*
 #else
   #define MOD_ERROR_VAL
